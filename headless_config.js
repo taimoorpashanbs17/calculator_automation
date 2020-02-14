edaa856cd4898380313de9753b5f9e2fa2b70905
taimoorpashanbs17 <taimoor.pasha@northbaysolutions.net>
2020-02-14 18:45:56 +0500

    Headless Config File has been Added

---
+++ headless_config.js
@@ -0,0 +1,17 @@
+exports.config = {
+    capabilities: {
+        'directConnect': true,
+        'browserName': 'chrome',
+        chromeOptions: {
+            args: ["--headless", "--disable-gpu", "--window-size=800x600"]
+        }
+    },
+    framework: 'jasmine',
+    specs: ['./specs/**/*.js'],
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 90000
+    },
+    onPrepare: function () {
+        browser.manage().timeouts().implicitlyWait(20000);
+    }
+};
